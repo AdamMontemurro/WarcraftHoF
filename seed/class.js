0e44828737e0042430ef8e07e67a13d40175f0f6
adam <adammontemurro@gmail.com>
2023-02-09 14:36:59 -0500

    added hunter seed

--- seed/class.js
+++ seed/class.js
@@ -15,8 +15,51 @@ const main = async () => {
       specs: 'Protection, Arms, Fury',
       img: 'placeholder'
   })
+  warrior.save()
+
+  const paladin = await new Class(
+    {
+      name: 'Paladin',
+      role: 'Tank/DPS/Healer',
+      primary_attribute: 'Strength/Intelect',
+      armor_type: 'Plate',
+      specs: 'Protection, Retribution, Holy',
+      img: 'placeholder'
+  })
+  paladin.save()
+
+  const hunter = await new Class(
+    {
+      name: 'Hunter',
+      role: 'DPS',
+      primary_attribute: 'Agility',
+      armor_type: 'Mail',
+      specs: 'Marksman, Survival, Beast Mastery',
+      img: 'placeholder'
+  })
+  hunter.save()
+
+  const paladin = await new Class(
+    {
+      name: 'Paladin',
+      role: 'Tank/DPS/Healer',
+      primary_attribute: 'Strength/Intelect',
+      armor_type: 'Plate',
+      specs: 'Protection, Retribution, Holy',
+      img: 'placeholder'
+  })
+  paladin.save()
+
+
+
+
 
 }
+
+
+
+
+
 
 const run = async () => {
   await main()
