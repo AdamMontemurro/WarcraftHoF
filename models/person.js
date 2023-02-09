187cdadfbafedfb4b42b61e4537a59ead82b4ac6
adam <adammontemurro@gmail.com>
2023-02-09 14:19:37 -0500

    Create person schema and exported model

---
+++ models/person.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const personSchema = new Schema(
+  {
+    name: {type:String, required: true},
+    race: {type:String, required: true},
+    class: {type:String, required: true},
+    status: {type:String, required: true},
+    involvements: {type:String, required: true},
+    img: {type:String, required: false},
+  }
+  {timestamps: true}
+)
+
+module.exports = mongoose.model('Class', personSchema)
