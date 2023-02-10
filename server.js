8f025e499c902f9d9f98cce0f7584fa04a37ea46
adam <adammontemurro@gmail.com>
2023-02-10 09:13:55 -0500

    sest up server.js

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const db = require('./db');
 const PORT = process.env.PORT || 3001;
 
 const app = express()
+app.use('/home', routes)
+
+db.on('error', console.error.bind(console, 'Connection Error '))
+
+app.listen(PORT, () => console.log(`Port: ${PORT}`))
