cf29b2cd8bb6792cc3fe4b543ac9467f360127e2
adam <adammontemurro@gmail.com>
2023-02-13 16:01:11 -0500

    create/delete comment controller/routes updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,40 +1,59 @@
-const {Class} = require('../models/')
-const {Comment} = require ("../models")
+const { Class } = require('../models/')
+const { Comment } = require("../models")
 
 
 const getClasses = async (req, res) => {
   try {
-      const classes = await Class.find()
-      return res.status(200).json({ classes })
+    const classes = await Class.find()
+    return res.status(200).json({ classes })
   } catch (error) {
-      return res.status(500).send(error.message);
+    return res.status(500).send(error.message);
   }
 }
 
 const getClassById = async (req, res) => {
   try {
+    const { id } = req.params;
+    const classA = await Class.findById(id)
+    if (classA) {
+      return res.status(200).json({ classA });
+    }
+    return res.status(404).send('That class does not exist');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+
+const newComment = async (req, res) => {
+  try {
+    const newComment = await new Comment(req.body)
+    await newComment.save()
+    return res.status(201).json({ newComment })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const deleteComment = async (req, res) => {
+  try {
       const { id } = req.params;
-      const classA = await Class.findById(id)
-      if (classA) {
-          return res.status(200).json({ classA });
+      const deleted = await Comment.findByIdAndDelete(id)
+      if (deleted) {
+          return res.status(200).send("Comment Deleted");
       }
-      return res.status(404).send('That class does not exist');
+      throw new Error("Comment not found");
   } catch (error) {
       return res.status(500).send(error.message);
   }
 }
 
-const newComment = async (req,res) => {
-  try {
-  const newComment = await new Comment(req.body)
-  await newComment.save()
-  return res.status(201).json({newComment})
-  } catch(error) {res.status(500).json({ error: error.message}) } 
-}
+
+
 
 
 module.exports = {
   getClasses,
   getClassById,
-  newComment
+  newComment,
+  deleteComment
 }
